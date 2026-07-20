3f2f645652a5c017f399fa467637fafe6bce104a
Matheus <matheusfyokoyama@gmail.com>
2026-07-19 22:36:56 -0300

    criando projeto sistema de pedidos

---
+++ sistema-pedidos/src/main/java/sistema/pedidos/com/br/SistemaPedidosApplication.java
@@ -0,0 +1,13 @@
+package sistema.pedidos.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPedidosApplication.class, args);
+	}
+
+}
---
+++ sistema-pedidos/src/test/java/sistema/pedidos/com/br/SistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.pedidos.com.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
